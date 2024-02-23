21a8cc617eaac2274ce7122d29903961fff761cf
Theo Hollweg <thollweg@mailbox.org>
2024-02-23 18:54:46 +0100

    Close DropdownMenu when clicked outside

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { onClickOutside } from "@vueuse/core";
 
+const menu = ref(null);
 const showMenu = ref<boolean>(false);
 
-const toggleShowMenu = () => (showMenu.value = !showMenu.value);
+const toggleShowMenu = (e: MouseEvent) => {
+  e.stopPropagation();
+  showMenu.value = !showMenu.value;
+};
+
+onClickOutside(menu, (event) => (showMenu.value = false));
 </script>
 
 <template>
-  <div class="relative">
+  <div ref="menu" class="relative">
     <span @click="toggleShowMenu" class="p-1">
       <slot name="button"></slot>
     </span>
     <ul
+      @click="toggleShowMenu"
       v-if="showMenu"
       class="absolute left-1/2 -translate-x-1/2 mt-2 bg-white border w-max z-10 rounded-md drop-shadow"
     >
